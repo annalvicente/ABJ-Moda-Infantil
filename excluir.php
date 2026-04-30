73d718ea376b0ba471660b6bb2aa9390d7046d54
annalvicente <anna.l.vicente@aluno.senai.br>
2026-04-30 11:40:09 -0300

    roupas

--- excluir.php
+++ excluir.php
@@ -4,8 +4,13 @@ include 'conexão.php';
 $id = $_GET['id'];
 
 if ($id) {
+   
+    $conn->query("DELETE FROM favoritos WHERE id_produto = $id");
+    $conn->query("DELETE FROM carrinho WHERE id_produto = $id");
+
     $conn->query("DELETE FROM produtos WHERE id = $id");
 }
 
 header("Location: tfuncionarios.php");
+exit;   
 ?>
